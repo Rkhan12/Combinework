ce9ec4cf0a5b1bf0d9b0de70ba67a0e9a64599b0
RKhan12 <raheelzafar.cdz@gmail.com>
2017-02-10 15:37:22 +0500

    text view

--- app/src/main/java/com/example/ninesol/combinework/MainActivity.java
+++ app/src/main/java/com/example/ninesol/combinework/MainActivity.java
@@ -4,10 +4,12 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
 public String push;
+    TextView tvName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
